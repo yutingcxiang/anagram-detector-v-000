53100895e91c06511bdf7ca1827fc17058242728
Christine Xiang <yutingcxiang@gmail.com>
2018-07-13 22:55:52 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  @@matches
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    letters = @word.split("")
+    letters_sorted = letters.sort
+    array_words.each do |item|
+      item_letters = item.split("")
+      item_letters_sorted = item_letters.sort
+      if item_letters_sorted == letters_sorted
+    end
+  end
+end
