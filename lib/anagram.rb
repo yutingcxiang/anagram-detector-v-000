7060926622ff41c445bbaf6a56ce6a1d4daa71ce
Christine Xiang <yutingcxiang@gmail.com>
2018-07-13 23:04:09 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
   def match(array)
     letters = @word.split("")
     letters_sorted = letters.sort
-    array.each do |item|
+    array.map do |item|
       item_letters = item.split("")
       item_letters_sorted = item_letters.sort
       if item_letters_sorted == letters_sorted
