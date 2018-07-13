5e269c55c11b8aa69f54d693726be00900267328
Christine Xiang <yutingcxiang@gmail.com>
2018-07-13 23:05:46 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    matches = []
+    letters = @word.split("")
+    letters_sorted = letters.sort
+    array.map do |item|
+      item_letters = item.split("")
+      item_letters_sorted = item_letters.sort
+      if item_letters_sorted == letters_sorted
+        matches << item
+      end
+    end
+    matches
+  end
+end
